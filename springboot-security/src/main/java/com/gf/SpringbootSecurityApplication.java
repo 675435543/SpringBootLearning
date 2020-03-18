1cdf9b1fe56977e6521ca82165ae736a98cc7c78
javahiker <675435543@qq.com>
2020-03-18 15:44:28 +0800

    '启动报错'

--- springboot-security/src/main/java/com/gf/SpringbootSecurityApplication.java
+++ springboot-security/src/main/java/com/gf/SpringbootSecurityApplication.java
@@ -2,8 +2,10 @@ package com.gf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.gf.mapper"})
 public class SpringbootSecurityApplication {
 
     public static void main(String[] args) {
